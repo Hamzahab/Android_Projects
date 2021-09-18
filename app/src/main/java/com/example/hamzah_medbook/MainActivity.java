c867240427a329b9672289498ad0e8f65d31e173
Hamzah <hamzah@ualberta.ca>
2021-09-17 21:12:13 -0600

    need to make changes, rough main activity w/ 3 parameters working?

--- app/src/main/java/com/example/hamzah_medbook/MainActivity.java
+++ app/src/main/java/com/example/hamzah_medbook/MainActivity.java
@@ -3,35 +3,59 @@ package com.example.hamzah_medbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    ListView lv;
+    ArrayAdapter<ArrayList<String>> aAdapter;
+//    ArrayAdapter<String> aAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //create medicine book
+        MedicineBook mb = new MedicineBook();
+
+        //create medicine
+        Medicine med1 = new Medicine("2021-09-25" ,"cetirizine hydrochloride", 5, "mg", 1);
+
+        Medicine med2 = new Medicine("2021-09-25" ,"ha", 5, "mg", 1);
+        Medicine med3 = new Medicine("2021-09-25" ,"haha", 5, "mg", 1);
+
+        mb.addMedicine(med1);
+        mb.addMedicine(med2);
+        mb.addMedicine(med3);
+
         //creating list view for main activity
-        ListView lv = (ListView) findViewById(R.id.main_listview);
+        lv = findViewById(R.id.main_listview);
 
-        //test string
-        List<String> testArrayList = new ArrayList<String>();
-        testArrayList.add("foo");
-        testArrayList.add("bar");
-        testArrayList.add("really testing");
-
+        String[] test = {"1", "2", "3"};
         //adapt to o/p
-        ArrayAdapter<String> aAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1
-                , testArrayList);
+//        aAdapter = new ArrayAdapter<>(this,
+//               R.layout.list_row_view
+//                , mb.getMedicineList());
+        aAdapter = new ArrayAdapter<ArrayList<String>>(this,
+                R.layout.list_row_view,
+                R.id.text1,
+                mb.getParams());
         //set
         lv.setAdapter(aAdapter);
 
+        Button mainButton = findViewById(R.id.button);
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_add_medicine);
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/hamzah_medbook/MedicineBook.java
+++ app/src/main/java/com/example/hamzah_medbook/MedicineBook.java
@@ -1,4 +1,50 @@
 package com.example.hamzah_medbook;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+    Stores all medicine added by user, keeping track of total doses daily
+ */
 public class MedicineBook {
+    private List<Medicine> medicineList;
+    private static int totalMedDose;
+
+    MedicineBook() {
+        this.medicineList = new ArrayList<>();
+    }
+
+    public List<Medicine> getMedicineList() {
+        return medicineList;
+    }
+
+    public void addMedicine(Medicine m) {
+        medicineList.add(m);
+
+        //increment totalMedDose
+        totalMedDose+=m.getDoseAmount();
+    }
+
+    public void removeMedicine(Medicine m) {
+        medicineList.remove(m);
+
+        //decrement totalMedDose
+        totalMedDose-=m.getDoseAmount();
+    }
+
+    public int getTotalMedDose(){
+        return totalMedDose;
+    }
+
+    public List<ArrayList<String>> getParams(){
+        List<ArrayList<String>> params = new ArrayList<>();
+        for(Medicine m:getMedicineList()){
+            ArrayList<String> test = new ArrayList<>();
+            test.add(m.getName());
+            test.add(m.getDateStarted());
+            test.add(Integer.toString(m.getDoseAmount()));
+            params.add(test);
+        }
+        return params;
+    }
 }
